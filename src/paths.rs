28833752da0283c8e67a6313481f21fb346fb54e
Zheng Luo <luozheng2002@sjtu.edu.cn>
2026-01-20 15:33:14 -0800

    added modified dataset

--- src/paths.rs
+++ src/paths.rs
@@ -1,10 +1,10 @@
 use std::{path::PathBuf, sync::LazyLock};
 
 pub static BASE_DATASET_PATH: LazyLock<PathBuf> =
-    LazyLock::new(|| PathBuf::from("ACEBench/data_all/data_en"));
+    LazyLock::new(|| PathBuf::from("ACEBench/data_all/data_en_modified"));
 
 pub static BASE_GROUND_TRUTH_PATH: LazyLock<PathBuf> =
-    LazyLock::new(|| PathBuf::from("ACEBench/data_all/data_en/possible_answer_hygienic"));
+    LazyLock::new(|| PathBuf::from("ACEBench/data_all/data_en_modified/possible_answer_hygienic"));
 
 pub static BASE_OUTPUT_PATH: LazyLock<PathBuf> =
     LazyLock::new(|| PathBuf::from("result_all/result_en"));
